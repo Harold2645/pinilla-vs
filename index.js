f47fdd0f644c1508d7a9bd0a47fa44467493dbb8
gloria valencia <yo.ya.07@hotmail.com>
2023-11-01 09:59:49 -0500

    Revert "sexto"
    This reverts commit 4cfc234b571ece06af59100d2dfdc4c23d95808a.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ sumarButton.addEventListener("click", function() {
     const numDos = parseFloat(numDosInput.value);
 
     if (!isNaN(numUno) && !isNaN(numDos)) {
-        const resultado = numUno - numDos;
+        const resultado = numUno + numDos;
         alert(`El resultado de la suma es: ${resultado}`);
     } else {
         alert("Por favor, ingrese números válidos en ambos campos.");
